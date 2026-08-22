16b24513ea921722c99b63f3715b4d98c03ff5a1
abdullahhelmy114 <abdullahhelmy114@gmail.com>
2026-08-22 11:02:19 +0300

    Fix CSP to allow inline scripts and Cloudflare Insights

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,9 @@ const securityHeaders = [
       "img-src 'self' data: blob: https://encrypted-tbn0.gstatic.com https://source.unsplash.com https://images.unsplash.com https://*.googleusercontent.com",
       "font-src 'self' https://fonts.gstatic.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-      // إزالة unsafe-inline و unsafe-eval من script-src حيثما أمكن، لكن Next.js قد يحتاج unsafe-eval في بيئات معينة.
-      "script-src 'self' https://apis.google.com https://www.gstatic.com https://*.firebaseio.com https://www.google.com",
+      // السماح بالسكربتات المضمنة (inline) وسكربتات Firebase/Google وCloudflare
+      "script-src 'self' 'unsafe-inline' https://apis.google.com https://www.gstatic.com https://*.firebaseio.com https://www.google.com https://static.cloudflareinsights.com",
+      // يمكن تحديد script-src-elem كذلك، لكن يكفي script-src
       "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://*.google-analytics.com https://generativelanguage.googleapis.com",
       "frame-src 'self' https://www.youtube.com https://*.firebaseapp.com https://*.google.com",
       "frame-ancestors 'self'",
